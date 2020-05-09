cb84f18dec7a9ba7fc4e6b55bf413fb72279f472
Dev Boldly <devboldly@gmail.com>
2020-05-09 13:39:55 -0500

    update comments

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // Google site verification for Docz
-const googleSiteVerification = undefined; //'abcdefg123456789hijk_6j4eSThtszNtFH9QRBKSlY';
+const googleSiteVerification = undefined; //'abcdefg123456789hijk_0a9b8c7d6e5f4g3h2i1j0k';
 
 export const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
   if (googleSiteVerification) {
