fb982c2784dda76dbfb6863ef15cf22b159b8ceb
Justin Mahar <contact@justinmahar.com>
2021-02-28 14:54:57 -0600

    Add prettierrc docs

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,8 @@
 /**
- * See: https://www.robertcooper.me/using-eslint-and-prettier-in-a-typescript-project
+ * Prettier Configuration: https://prettier.io/docs/en/configuration.html
+ * Prettier Options: https://prettier.io/docs/en/options.html
+ *
+ * For a post on how everything is set up, see: https://www.robertcooper.me/using-eslint-and-prettier-in-a-typescript-project
  */
 module.exports = {
   semi: true,
