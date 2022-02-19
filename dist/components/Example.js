9af0afa92ebbca6a0df6b3b78e5a94d48e1ea4f4
Justin Mahar <contact@justinmahar.com>
2022-02-19 09:30:04 -0500

    Build, version, and publish.

--- dist/components/Example.js
+++ dist/components/Example.js
@@ -1,4 +1,15 @@
 "use strict";
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -8,7 +19,8 @@ const react_1 = __importDefault(require("react"));
 /**
  * This is the description for the Example component
  */
-const Example = ({ backgroundColor, label, ...props }) => {
-    return (react_1.default.createElement("div", { ...props, style: { backgroundColor, ...props.style } }, label));
+const Example = (_a) => {
+    var { backgroundColor, label } = _a, props = __rest(_a, ["backgroundColor", "label"]);
+    return (react_1.default.createElement("div", Object.assign({}, props, { style: Object.assign({ backgroundColor }, props.style) }), label));
 };
 exports.Example = Example;
--- dist/stories/Example.stories.js
+++ dist/stories/Example.stories.js
@@ -4,21 +4,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.World = exports.Hello = void 0;
+/*
+ * More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+ * More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+ * More on args: https://storybook.js.org/docs/react/writing-stories/args
+ * More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+ */
 const react_1 = __importDefault(require("react"));
 const Example_1 = require("../components/Example");
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 exports.default = {
     title: 'Components/Example',
     component: Example_1.Example,
-    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-    argTypes: {
-        backgroundColor: { control: 'color' },
-    },
 };
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template = (args) => react_1.default.createElement(Example_1.Example, { ...args });
+const Template = (args) => react_1.default.createElement(Example_1.Example, Object.assign({}, args));
 exports.Hello = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 exports.Hello.args = {
     label: 'Hello',
 };
