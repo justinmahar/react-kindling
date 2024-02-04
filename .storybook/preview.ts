6d2194be6d07069b03fcc2ca4d7ebdbad8c944ae
Justin Mahar <contact@justinmahar.com>
2024-02-04 15:53:57 -0500

    Cleanup

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,9 @@
 // [lock-all/] 🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫
 import { Preview } from '@storybook/react';
 
+// Uncomment to enable Bootstrap (First run: npm i bootstrap --save-dev):
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
