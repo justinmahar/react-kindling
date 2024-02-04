d79bc23ce891bf7cf8236ec8364a21a798032fba
Justin Mahar <contact@justinmahar.com>
2024-02-04 15:33:56 -0500

    Remove dist

--- dist/stories/Example.stories.d.ts
+++
@@ -1,4 +0,0 @@
-declare const _default: import("@storybook/types").ComponentAnnotations<import("@storybook/react/dist/types-0fc72a6d").R, import("../components/Example").ExampleProps>;
-export default _default;
-export declare const Hello: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react/dist/types-0fc72a6d").R, import("../components/Example").ExampleProps>;
-export declare const World: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react/dist/types-0fc72a6d").R, import("../components/Example").ExampleProps>;
