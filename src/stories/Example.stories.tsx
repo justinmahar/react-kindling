bb0bd460faae4f5fcb2532614fde91ca55645147
Justin Mahar <contact@justinmahar.com>
2024-02-05 09:42:43 -0500

    Update example story

--- src/stories/Example.stories.tsx
+++ src/stories/Example.stories.tsx
@@ -1,27 +1,19 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { Example } from '../components/Example';
+import { StoryDecorator } from './core/StoryDecorator';
 const StoryComponent = Example;
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+// For help writing stories, see: https://storybook.js.org/docs/writing-stories
 const meta = {
   title: 'Stories/Example',
   component: StoryComponent,
-  parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'centered',
-  },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   // tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-    // backgroundColor: { control: "color" },
-  },
+  decorators: [StoryDecorator],
 } satisfies Meta<typeof StoryComponent>;
-
 export default meta;
 type Story = StoryObj<typeof meta>;
-// For help writing stories, see: https://storybook.js.org/docs/writing-stories
 
 export const Hello: Story = {
   args: {
