ac62864a855ec79b045ec2764e22dfd0621249ff
Justin Mahar <contact@justinmahar.com>
2024-02-08 15:55:22 -0500

    Update license

--- src/stories/Example.stories.tsx
+++ src/stories/Example.stories.tsx
@@ -2,10 +2,13 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Example } from '../components/Example';
 
 // === Setup ===
-const StoryComponenet = Example; // <-- Set to your component
-const meta: Meta<typeof StoryComponenet> = {
+const StoryComponent = Example; // <-- Set to your component
+const meta: Meta<typeof StoryComponent> = {
   title: 'Stories/Example', // <-- Set to your story title
-  component: StoryComponenet,
+  component: StoryComponent,
+  parameters: {
+    options: { showPanel: false }, // Don't show addons panel
+  },
 };
 export default meta;
 type Story = StoryObj<typeof meta>;
